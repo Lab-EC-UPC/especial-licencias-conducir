fcd14a24968c3387767b3df95d67e13ce6b2c0af
sayufu <lilianafuye@gmail.com>
2025-07-11 23:57:33 -0500

    added peru map

--- src/features/02-Map/MapSection.tsx
+++ src/features/02-Map/MapSection.tsx
@@ -1,10 +1,12 @@
+import {PeruMap} from "./components/PeruMap.tsx";
+
 export const MapSection = () => {
 
   return (
     <div className="bg-violet-200">
       <div className="mx-auto container w-full h-screen">
-        <div className="flex items-center justify-center text-center h-full">
-          Radiografía nacional
+        <div className="flex items-center justify-center text-center h-full py-8">
+          <PeruMap />
         </div>
       </div>
     </div>
