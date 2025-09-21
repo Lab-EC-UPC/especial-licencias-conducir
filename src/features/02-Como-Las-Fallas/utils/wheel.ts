64f859f2f77a8184650eaf5348725b53ef95d5b2
Daniel Zapata <danizt30@gmail.com>
2025-09-21 11:26:33 -0500

    Correcciones de redacción

--- src/features/02-Como-Las-Fallas/utils/wheel.ts
+++ src/features/02-Como-Las-Fallas/utils/wheel.ts
@@ -8,27 +8,27 @@ export interface WheelProps {
 const items: WheelProps[] = [
     {
         question: "Al cambiar de dirección, un conductor debe:",
-        title: "30,324",
+        title: "30.324",
         answer: "Cantidad cometida en los ultimos 4 años.",
-        count: "39,7875",
+        count: "39.7875",
     },
     {
         question: "El servicio de transporte terrestre, por el elemento transportado, se clasifica en:",
         title: "454",
         answer: "Cantidad cometida en los ultimos 4 años.",
-        count: "238,867",
+        count: "238.867",
     },
     {
         question: "Las marcas en el pavimento constituyen un elemento indispensable para la operación vehicular, pues su función es:",
-        title: "311,941",
+        title: "311.941",
         answer: "Cantidad cometida en los ultimos 4 años.",
-        count: "140,582",
+        count: "140.582",
     },
     {
         question: "La siguiente señal (P-5-1A) / (P(5-46)), le advierte al conductor que:",
-        title: "259,867",
+        title: "259.867",
         answer: "Cantidad cometida en los ultimos 4 años.",
-        count: "137,077",
+        count: "137.077",
     },
     {
         question: "Siempre que no exista una señal de limite de velocidad, en zonas urbanas el limite de velocidad en calles y jirones es de:",
